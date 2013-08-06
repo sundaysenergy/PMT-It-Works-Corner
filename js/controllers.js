14061c653d0124fcfb3cef19f0b928d188ec41eb
Tim Lupfer <tim@sundaysenergy.com>
2013-08-06 14:35:54 -0500

    Add attribute prices

--- js/controllers.js
+++ js/controllers.js
@@ -10,18 +10,46 @@ function PmtProductList($scope, $http) {
   $scope.units = 0;
   $scope.dollars = 0.00;
   
+  $scope.configuration = [];
+  
   $scope.addCart = function(nid, quantity, title, price) {
     var attr = [];
     angular.copy($scope.attributes, attr);
     $scope.units = $scope.units + quantity;
-    $scope.dollars = (parseFloat($scope.dollars) + parseFloat((quantity * price))).toFixed(2); 
     $scope.cart.push({'price': price, 'title': title, 'nid':nid, 'quantity': quantity, 'attributes': attr });
+    $scope.dollars = $scope.totalCart();
     $scope.attributes.length = 0;
   }
   
+  $scope.totalCart = function() {
+    var cartitems = [];
+    angular.copy($scope.cart, cartitems);
+    var length = cartitems.length;
+    var dollars = 0;
+    console.log(cartitems);
+    
+    for (var i = 0; i < length; i++) {
+      var price = cartitems[i].price;
+      var quantity = cartitems[i].quantity;
+      var addons = 0;
+      var attribute_length = cartitems[i].attributes.length;
+      for (var j = 0; j < attribute_length; j++) {
+        addons = parseFloat(addons) + parseFloat(cartitems[i].attributes[j].price);
+      }
+      dollars = parseFloat(dollars) + parseFloat((quantity * (price + addons)));
+    }
+    return dollars.toFixed(2);
+  }
+  
   $scope.removeItem = function(index, quantity, price) {
-    $scope.dollars = (parseFloat($scope.dollars) - parseFloat(quantity * price)).toFixed(2);
     $scope.units = $scope.units - quantity;
     $scope.cart.splice(index, 1);
+    $scope.dollars = $scope.totalCart();
   }
+
+/*
+  $scope.recalc = function(select_model) {
+    
+  }
+*/
 }
