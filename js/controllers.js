0cbcd442fd465c95a0fe989cfc05022c64db7a25
Tim Lupfer <tim@sundaysenergy.com>
2013-08-05 19:46:59 -0500

    Bootstrap

--- js/controllers.js
+++ js/controllers.js
@@ -7,10 +7,13 @@ function PmtProductList($scope, $http) {
 
   $scope.cart = [];
   $scope.attributes = [];
+  $scope.units = 0;
+  $scope.dollars = 0;
   
   $scope.addCart = function(nid, quantity) {
     var attr = [];
     angular.copy($scope.attributes, attr);
+    $scope.units = $scope.units + quantity;
     $scope.cart.push({'nid':nid, 'quantity': quantity, 'attributes': attr });
     $scope.attributes.length = 0;
   }
